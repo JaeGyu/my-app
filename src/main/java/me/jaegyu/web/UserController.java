90a0466a4f1788680cef400e8999256a342532fa
icpu <icpu@naver.com>
2017-03-06 19:57:22 +0900

    코드 리팩토링

--- src/main/java/me/jaegyu/web/UserController.java
+++ src/main/java/me/jaegyu/web/UserController.java
@@ -3,25 +3,34 @@ package me.jaegyu.web;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import me.jaegyu.domain.User;
+import me.jaegyu.domain.UserRepository;
 
 @Controller
+@RequestMapping("")
 public class UserController {
 	private List<User> users = new ArrayList<>();
+	
+	@Autowired
+	private UserRepository userRepositiry;
 
-	@PostMapping("/create")
+	@PostMapping("")
 	public String create(User user) {
 		System.out.println(user);
-		users.add(user);
-		return "redirect:/list";
+		userRepositiry.save(user);
+		return "redirect:/users/list";
 	}
 
-	@GetMapping("/list")
+	@GetMapping("/users")
 	public String list(Model model) {
-		model.addAttribute("users",users);
-		return "list";
+		model.addAttribute("users",userRepositiry.findAll());
+		return "/user/list";
 	}
 }
