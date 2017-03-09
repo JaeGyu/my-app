7e97eff7eea93886833ea6eb6d12b936a8fd2711
icpu <icpu@naver.com>
2017-03-09 20:54:52 +0900

    질문목록 구현

--- src/main/java/me/jaegyu/web/HomeController.java
+++ src/main/java/me/jaegyu/web/HomeController.java
@@ -1,13 +1,22 @@
 package me.jaegyu.web;
 
+import javax.annotation.Resource;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import me.jaegyu.domain.QuestionRepository;
 
 @Controller
 public class HomeController {
-	
+
+	@Resource
+	private QuestionRepository repository;
+
 	@RequestMapping("/")
-	public String index(){
+	public String index(Model model) {
+		model.addAttribute("questions", repository.findAll());
 		return "index";
 	}
 }
